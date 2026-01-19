0fdb66d1d5044216877ea63c2f4e6a917f2e9d32
Zarko Gavric <zarko.gavric@sportserve.co>
2026-01-19 14:22:37 +0100

    Added vue for frontend rendering

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,25 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import WelcomeItem from "@/components/WelcomeItem.vue";
+import Products from "@/views/Products.vue";
+
+const routes = [
+    { path: '/:locale(en|sr)', component: Products },
+    { path: '/hello-world', component: HelloWorld },
+    { path: '/welcome-item', component: WelcomeItem },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
